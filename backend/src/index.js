67ace2e66125fd85ca00f8b7bf7dbea7926eec9e
Kheil <cunanankheilanthony@gmail.com>
2025-07-30 22:27:48 +0800

    CORS update

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://library-management-react-five.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json()); // Parse JSON payloads
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded payloads (for forms)
 
